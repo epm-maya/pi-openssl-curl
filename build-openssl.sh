be1048263ab51a3ad9a58a7c6118bfa286d79e06
Noshiro Nanai <epm.maya@gmail.com>
2021-02-15 18:18:18 +0900

    Fix cross tools

--- build-openssl.sh
+++ build-openssl.sh
@@ -4,6 +4,14 @@ set -ex
 
 export TARGET_HOST=arm-linux-gnueabihf
 
+export AR=$TARGET_HOST-ar
+export AS=$TARGET_HOST-as
+export CC=$TARGET_HOST-gcc
+export CXX=$TARGET_HOST-g++
+export LD=$TARGET_HOST-ld
+export RANLIB=$TARGET_HOST-ranlib
+export STRIP=$TARGET_HOST-strip
+
 pushd openssl
 
 ./Configure linux-generic32 no-shared \
